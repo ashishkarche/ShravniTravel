21b9b64f41669aa7f83543cc466ce609e2ae8193
ashishkarche <ashishkarche9@gmail.com>
2024-07-24 11:40:35 +0530

    v2.0

---
+++ js/book_car.js
@@ -0,0 +1,12 @@
+
+function openWhatsApp() {
+    const phoneNumber = '988140654'; // Replace with your company's phone number in international format
+    const message = 'Hello, I would like to inquire about your services.'; // Replace with your custom message
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+}
+
+function openPhone() {
+    const phoneNumber = '+91988140654'; // Replace with your company's phone number in international format
+    window.open(`tel:${phoneNumber}`);
+}
